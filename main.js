3d8c7987b401fecb1b5c9fdd4638d4853699eb49
SNAFF <snaffs.pd.25@gmail.com>
2022-12-20 21:36:11 +0700

    no

--- main.js
+++ main.js
@@ -10,10 +10,8 @@ const index_r = require('./routes/index');
 const user_r = require('./routes/user');
 const store_r = require('./routes/store');
 const backend_r = require('./routes/backend');
-
 app.use(express.static("./public"));
 app.use(cookieParser());
-
 app.use(sessions({
     secret: '=_oprcrinzcsnaff',
     saveUninitialized: true,
@@ -22,15 +20,12 @@ app.use(sessions({
 }))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json());
-
 app.set('view engine', 'ejs');
 app.set('views', './views');
-
 app.use('/', index_r);
 app.use('/user', user_r);
 app.use('/store', store_r);
 app.use('/backend', backend_r);
- 
 app.listen(80, ()=>{
-    console.log('ok on 80');
+    console.log('ok on 80 http://app.riz');
 }) ;
