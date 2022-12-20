06d31d7afa36a9eeac4333b9bdb67f1394c0c91d
SNAFF <snaffs.pd.25@gmail.com>
2022-12-20 19:00:42 +0700

    add buy, md5, reload_on_index

--- libs/store.js
+++ libs/store.js
@@ -4,45 +4,33 @@ const uri = "mongodb://data.app.riz:4566/"
 const client = new MongoClient(uri)
 const dataBase = client.db('shop_store');
 
+const cmd = new(require('./cmd'))(dataBase);
 
-// func main
-const find = async (coll, data) => {
-    try {
-        let catchs = [];
-        (await dataBase.collection(coll).find(data).toArray()).forEach(item => {
-            if ( item != null) {
-                catchs.push(item)
-            }
-        }); return catchs;
-    } catch (e) {
-        console.error(e);
-    }
-};
-const insertOne = async ( coll, data ) => {
-    try {
-        (await dataBase.collection( coll ).insertOne(data))
-    } catch (e) {
-        console.error(e);
-    }
-}
-// func main
+
 
 const store= {
     async get(t){
         if ( t == null){
             t = {}
         };
-        x = await find('items', t)
+        x = await cmd.findOne('items', t)
         return x
     },
     async add(t) {
-        return await insertOne('items', t)
+        return await cmd.insertOne('items', t)
     },
     async buy(id, session) {
         res = await this.get({_id: ObjectId(id)})
         value = res[0].data.value
-        console.log(`${session.osb} : ${value}`)
-        console.log(res[0])
+        // console.log(`${session.osb} : ${value}`)
+        // console.log(res[0])
+        if(res[0]!=null && (session.osb>=value)!=false ){
+            console.log('success')
+            // return [cmd.deleteOne('items', {_id: ObjectId(id)}), value]
+            return [true, value]
+        } else {
+            return [session.osb>=value];
+        }
     }
 }
 
---
+++ routes/utils.js
@@ -0,0 +1,31 @@
+const {user} = require('../libs/db');
+xlogin=async (req, iden)=>{
+    let data = {
+        name: iden.name,
+        password: iden.password,
+    }
+    // console.log("on login")
+    var c = user.login(data)
+    await c.then(r=>{ 
+        if(r==null){
+            res.redirect('/user?error=1')
+        } else {
+            let data = {}
+            for ( i in r) {
+                data[i] = r[i]
+            }
+            // req.session.perm = data.perm
+            // req.session.name = data.name
+            // req.session.email = data.email
+            // req.session.created = data.created
+            // req.session.osb = data.osb
+            for (x in data){
+                if (x != '_id') {
+                    req.session[x] = data[x]
+                }
+            }
+        }
+    })
+}
+
+module.exports = {xlogin};
